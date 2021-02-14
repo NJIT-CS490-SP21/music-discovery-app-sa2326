666774f88af72742fbfd60d0581adf304cfd0f20
EC2 Default User <ec2-user@ip-172-31-42-108.ec2.internal>
2021-02-14 06:48:11 +0000

    Added more artists and improved lyrics functionality with cleaner UI

--- music.py
+++ music.py
@@ -9,20 +9,20 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def music_discovery():
-    rand = random.randint(0,2)
+    rand = random.randint(0,8)
     #In respective order: Bazzi, Frocious, Pink Sweat$
-    artists = ['4GvEc3ANtPPjt1ZJllr5Zl', '0W96fH9CZR4WRGI4x3iBJs', '1W7FNibLa0O0b572tB2w7t']
+    artists = [ '4GvEc3ANtPPjt1ZJllr5Zl', '0W96fH9CZR4WRGI4x3iBJs', '1W7FNibLa0O0b572tB2w7t',
+        '2pAWfrd7WFF3XhVt9GooDL', '2s1fodCLf7tb0bogSUNBqY', '0TImkz4nPqjegtVSMZnMRq', '68DWke2VjdDmA75aJX5C57',
+        '6gWGD0yeQYobb2sq0LUr7k', '5eIbEEQnDM8yuDVB0bimSP']
     my_music=get_music_data(artists[rand])
-    if rand==0:
-        my_lyrics_url=get_lyrics('I.F.L.Y')
-    elif rand==1:
-        my_lyrics_url=get_lyrics('ilyw')
-    else:
-        my_lyrics_url=get_lyrics('At My Worst')
+    music_data=my_music['data']
+    song_title=music_data[0]
+    
+    my_lyrics_url=get_lyrics(song_title)
     
     return render_template(
         "index.html",
-        all_data=my_music['data'],
+        all_data=music_data,
         lyrics=my_lyrics_url['lyrics_url']
     )
 
@@ -31,3 +31,4 @@ app.run(
     host=os.getenv('IP', '0.0.0.0'),
     debug=True
 )
+
